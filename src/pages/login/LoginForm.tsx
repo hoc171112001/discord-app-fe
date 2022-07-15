062ffc6459da5a0d2ee5aacefbdb05da3e2734a8
VuLong <vulongpt@gmail.com>
2022-07-15 17:24:20 +0700

    add logic to login page and perfect each px

--- src/pages/login/LoginForm.tsx
+++ src/pages/login/LoginForm.tsx
@@ -2,6 +2,7 @@ import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 import { Box, Button, Grid, TextField } from '@mui/material';
 import { Controller, useForm } from 'react-hook-form';
 import * as yup from 'yup';
+import { QRCodeCanvas } from 'qrcode.react';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { Link } from 'react-router-dom';
 
@@ -18,32 +19,30 @@ function LoginForm() {
     control,
     formState: { errors },
   } = useForm({ resolver: yupResolver(LoginSchema) });
+
+  const handleSubmitForm = (values: any) => {
+    console.log('values :', values);
+  };
+
   return (
-    <Grid container zIndex={100} spacing={2} className="loginForm" sx={{ padding: '2rem' }}>
+    <Grid container zIndex={100} spacing={4} className="loginForm" sx={{ padding: '2rem' }}>
       <Grid xs={8}>
         <Button variant="text" color="inherit" startIcon={<KeyboardArrowLeftIcon />}>
           Go back
         </Button>
         <h3 className="heading">Welcome back!</h3>
         <p className="description">We're so excited to see you again!</p>
-        <form action="" className="form">
+        <form action="" className="form" onSubmit={handleSubmit(handleSubmitForm)}>
+          <label title="">Enter your email or number</label>
           <Controller
             name="email"
             control={control}
             render={({ field }) => {
-              return (
-                <TextField
-                  {...field}
-                  type="text"
-                  variant="outlined"
-                  label="Email or phone number"
-                  color="info"
-                  fullWidth
-                />
-              );
+              return <TextField {...field} variant="outlined" fullWidth />;
             }}
             defaultValue=""
           />
+          <label title="">Password</label>
           <Controller
             name="password"
             control={control}
@@ -53,16 +52,14 @@ function LoginForm() {
                   {...field}
                   type="password"
                   variant="outlined"
-                  label="Password"
                   fullWidth
                   className="password"
-                  sx={{ marginTop: 4 }}
                 />
               );
             }}
             defaultValue=""
           />
-          <Link to="/forgotpassword" className="forgotPass">
+          <Link to="/forgotpassword" className="forgotPass auth-link">
             Forgot password
           </Link>
           <Button type="submit" fullWidth variant="contained" sx={{ marginTop: 2 }}>
@@ -71,13 +68,37 @@ function LoginForm() {
         </form>
 
         <Box sx={{ marginTop: 2 }}>
-          <span>Need an account</span>
-          <Link className="register" to="/register">
+          <span>Need an account </span>
+          <Link className="register auth-link" to="/register">
             Register
           </Link>
         </Box>
       </Grid>
-      <Grid xs={4}>Loa</Grid>
+      <Grid item xs={4} className="flex-col-center">
+        <Box className="flex-center" component="div" sx={{ width: '100%', position: 'relative' }}>
+          <QRCodeCanvas
+            value="https://discord.com/downloadddddddddddd"
+            size={180}
+            bgColor="#fff"
+            style={{ border: '8px solid #fff', borderRadius: '0.5rem', position: 'relative' }}
+            level="H"
+          ></QRCodeCanvas>
+          <img
+            src="/assets/images/login/logo.png"
+            alt="logo discord"
+            className="login-logo"
+            width={50}
+          />
+        </Box>
+
+        <Box className="text-center login-qr-heading" component="h3">
+          Log in with QR Code
+        </Box>
+
+        <Box className="text-center login-qr-info">
+          Scan this with the <strong>Discord mobile app</strong> to log in instantly.
+        </Box>
+      </Grid>
     </Grid>
   );
 }
