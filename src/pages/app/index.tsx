af4a1301fde2649f6034117e3b19d3c74579a4bb
hochv <hoc17112001@gmail.com>
2022-08-05 16:00:32 +0700

    server list

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -6,6 +6,7 @@ import LoadingFullScreen from '../../components/LoadingFullScreen';
 function AppContainer() {
   const [loading, setLoading] = useState<boolean>(true);
   const [fadeout, setFadeout] = useState<boolean>(false);
+  const [focusing, setFocusing] = useState<any>('home');
   useEffect(() => {
     let timeout: any;
     mainApi
@@ -25,11 +26,44 @@ function AppContainer() {
     };
   }, []);
 
+  const listServer = ['PM', 'HM', 'HH', 'VL', 'XX', '.C', 'OM'];
+
   return (
     <div className="homepage">
       {loading && <LoadingFullScreen fadeout={fadeout} />}
       <div className="layout">
-        <div className="layout">layout</div>
+        <div className="aside">
+          <div className="server_list">
+            <ul>
+              <li
+                className={`server_items server_items--nobg personal-server ${
+                  focusing == 'home' && 'focused'
+                }`}
+                onClick={() => setFocusing('home')}
+              >
+                <img src="/assets/images/app/logo.svg" alt="" style={{ marginRight: '1px' }} />
+              </li>
+              <li className="separator"></li>
+              {listServer.map((e, idx) => (
+                <li
+                  className={`server_items server_items--nobg ${focusing == idx && 'focused'}`}
+                  key={idx}
+                  onClick={() => setFocusing(idx)}
+                >
+                  {e}
+                </li>
+              ))}
+              <li className="separator"></li>
+              <li className="server_items server_items--button personal-server ">
+                <img src="/assets/images/app/plus.svg" alt="" style={{ marginRight: '1px' }} />
+              </li>
+              <li className="server_items server_items--button personal-server ">
+                <img src="/assets/images/app/comunity.svg" alt="" style={{ marginRight: '1px' }} />
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="layout_main"></div>
       </div>
     </div>
   );
