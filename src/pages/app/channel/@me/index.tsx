0504648bfa64c0143726933acb274de3e7af1509
Hoang Van Hoc <hochv@i.hivetech.vn>
2022-08-17 21:38:06 +0700

    search bar

--- src/pages/app/channel/@me/index.tsx
+++ src/pages/app/channel/@me/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useEffect } from 'react';
+import { useModal } from '../../../../shared/Hooks/modal';
 
 interface IProps {}
 
@@ -8,14 +9,22 @@ interface IProps {}
  **/
 
 export const MeChannel: FC<IProps> = (props) => {
+  const modal = useModal();
   useEffect(() => {
     console.log('@me');
   }, []);
+  const onOpenSearchModal = () => {
+    modal.openModalConfirm('search', 'deo biet noi j', true);
+  };
   return (
     <div className="personal_wrapper">
       <div className="main_func_wrapper">
         <div className="main_func_wrapper__sidebar">
-          <div className="searchbar">searchbar</div>
+          <div className="searchbar">
+            <div className="search_btn" onClick={onOpenSearchModal}>
+              Search or start the conversation
+            </div>
+          </div>
           <div className="list__interactive">List Interactive</div>
           <div className="personal_controller">controller</div>
         </div>
