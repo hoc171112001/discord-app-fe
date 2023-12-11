c093f2ed39421342e6e6b5498187768deccd69c8
hochv <hochv2001@gmail.com>
2023-12-11 17:22:38 +0700

    snap

---
+++ components/Register/formBody/EmailAnoucement.tsx
@@ -0,0 +1,10 @@
+import { BaseClientComponent } from "@/types";
+import React, { FC } from "react";
+interface IProps extends BaseClientComponent {}
+/**
+ * @author
+ * @function @EmailAnnoucement
+ **/
+export const EmailAnnoucement: FC<IProps> = (props) => {
+  return <div>EmailAnnoucement</div>;
+};
